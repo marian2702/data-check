ca3433a7bfa68477982245684304d501db67bdb2
Halibei Marian <marianhalibei123@gmail.com>
2023-07-18 08:35:49 +0200

    Solved the first challenge of the bootcamp :)

--- demo.py
+++ demo.py
@@ -1,7 +1,21 @@
 """Dummy challenge for Kitt Demo"""
-
-
+import unittest
 
 def circle_area(radius):
-    """Returns the area of the circle of given radius"""
-    pass  # YOUR CODE HERE
+    return 3.14 * radius * radius
+
+class TestDemo(unittest.TestCase):
+    def test_returns_3_14_for_radius_1(self):
+        expected = 3.14
+        actual = circle_area(1)
+        self.assertAlmostEqual(expected, actual, 2)
+
+    def test_returns_78_5_for_radius_5(self):
+        expected = 78.5
+        actual = circle_area(5)
+        self.assertAlmostEqual(expected, actual, 1)
+
+    def test_returns_zero_for_negative_radius(self):
+        expected = 0
+        actual = circle_area(-1)
+        self.assertEqual(expected, actual)
